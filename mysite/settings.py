65a1c742c11786d5173e40740b96cc87644bf0a3
yuhona <201821010204@mail.bnu.edu.cn>
2019-02-21 21:09:42 +0800

    Add login in and login out

--- mysite/settings.py
+++ mysite/settings.py
@@ -81,6 +81,7 @@ DATABASES = {
     }
 }
 
+LOGIN_REDIRECT_URL = '/'
 
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
--- mysite/urls.py
+++ mysite/urls.py
@@ -25,9 +25,14 @@ from django.conf.urls import include, url
 from django.contrib import admin
 from django.urls import path
 
+from django.contrib.auth import views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    url(r'', include('blog.urls')),
+	path('accounts/login/', views.LoginView.as_view(), name='login'),
+    path('accounts/logout/', views.LogoutView.as_view(next_page='/'), name='logout'),
+	path('', include('blog.urls')),
+	
 ]
 
 
